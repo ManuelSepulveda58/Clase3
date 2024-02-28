0b8fb0b242a691cfc62d867f2c8581a35aa4cd68
ManuelSepulveda58 <160683067+ManuelSepulveda58@users.noreply.github.com>
2024-02-28 18:46:56 -0500

    Add files via upload

---
+++ Clase 3/clase3.js
@@ -0,0 +1,19 @@
+function sumar(a, b) {
+    return a + b;
+}
+let resultado = sumar(5, 3);
+console.log(resultado);
+
+let multiplicar = function(x, y) {
+    return x * y;
+};
+
+
+let resultadoMultiplicacion = multiplicar(4, 6);
+console.log(resultadoMultiplicacion); 
+
+let dividir = (a, b) => a / b;
+
+let resultadoDivision = dividir(8, 2);
+console.log(resultadoDivision); // Output: 4
+
